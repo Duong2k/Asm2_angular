ded609dd7f49258c88acb74c842daad7fa53d608
duongph03 <duongddph23612@fpt.edu.vn>
2023-05-31 15:55:38 +0700

    first commit

---
+++ admin/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+    { path: '/user', title: 'user',  icon:'pe-7s-user', class: '' },
+    { path: '/productmanagement', title: 'Product Management',  icon:'pe-7s-note2', class: '' },
+    { path: '/category', title: 'Category',  icon:'pe-7s-news-paper', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
